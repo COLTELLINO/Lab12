03acefe0ac5644647d3b1d658ea2beb0105220c9
Filippo Patrignani <filippo.patrignani64@gmail.com>
2025-01-05 16:36:43 +0100

    121 done

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,49 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final List<Integer> values = new ArrayList<>();
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		for (int i = 0; i < size; i++) {
+			this.values.add(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
-	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+	public List<Integer> values() {		
+		return Collections.unmodifiableList(values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.values().stream().map(n -> n < this.values.size()).toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int value = this.values.get(elem);
+		this.values.set(elem, ++value);
+		return value;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return values().stream().map(String::valueOf).collect(Collectors.joining("|","<<",">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return values().stream().distinct().count() == 1;
 	}
 }
